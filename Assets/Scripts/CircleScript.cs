17bc74342fe323430f13b5c07c377f47a66b525f
Vinayak agarwal <vinayakaggarwal05@gmail.com>
2021-08-16 12:39:38 +0530

    first commit

---
+++ Assets/Scripts/CircleScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private float _speed = 2f;
+
+    void Start()
+    {
+        transform.position = new Vector3(-1,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(1, Vertical, Horizontal) * Time.deltaTime * _speed);
+
+        if(transform.position.x > 16f)
+            {
+                transform.position = new Vector3(16f,transform.position.y,transform.position.z);
+            }
+
+            if(transform.position.x < -16f)
+            {
+                transform.position = new Vector3(-16f,transform.position.y,transform.position.z);
+            }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        // if(other.tag == "New Cube")
+        // {
+        //     transform.localScale += new Vector3(1,1,1);
+        //     Destroy(other.gameObject);
+        // }
+
+        if(other.tag == "Bad Cube")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("game over");
+        }
+    }
+}
